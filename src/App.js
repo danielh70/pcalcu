00551035b6e8cea8a9b19de614835622cbf38c6b
Danielh70 <danhook007@gmail.com>
2026-04-09 21:13:40 -0500

    Temporarily disable Panel and Tape Calc tabs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 // import PostLevel from './components/PostLevel';
-import TapeCalc from './components/TapeCalc';
+// TEMPORARILY DISABLED
+// import TapeCalc from './components/TapeCalc';
 import Systems from './components/Systems';
 
 import './App.css';
@@ -41,8 +42,8 @@ export default function App() {
           aria-label='Main navigation tabs'
         >
           {/* <Tab label='Post Level' id='tab-0' aria-controls='tabpanel-0' /> */}
-          <Tab label='Tape Calc' id='tab-0' aria-controls='tabpanel-0' />
-          <Tab label='Systems' id='tab-1' aria-controls='tabpanel-1' />
+          {/* TEMPORARILY DISABLED <Tab label='Tape Calc' id='tab-0' aria-controls='tabpanel-0' /> */}
+          <Tab label='Systems' id='tab-0' aria-controls='tabpanel-0' />
         </Tabs>
         <Divider sx={{ mb: 2 }} />
         {/* <TabPanel value={value} index={0}>
@@ -50,12 +51,14 @@ export default function App() {
             <PostLevel />
           </div>
         </TabPanel> */}
+        {/* TEMPORARILY DISABLED
         <TabPanel value={value} index={0}>
           <div className='tab-content-card'>
             <TapeCalc />
           </div>
         </TabPanel>
-        <TabPanel value={value} index={1}>
+        */}
+        <TabPanel value={value} index={0}>
           <div className='tab-content-card'>
             <Systems />
           </div>
